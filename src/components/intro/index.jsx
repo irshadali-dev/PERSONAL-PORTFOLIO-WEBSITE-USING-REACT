1701ffcc63ed9c7abbe91947455888cda31cb288
Irshad Ali <ia46994@gmail.com>
2024-10-28 08:59:49 +0530

    intro background create

--- src/components/intro/index.jsx
+++ src/components/intro/index.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import "./style.scss";
+import cloud from "../../images/cloud.png";
+import cloudSoft from "../../images/cloud-soft.png";
 
 const Intro = () => {
-    return <div>Intro</div>;
+    return (
+        <div className="Intro-section">
+            <div
+                className="vector-bg"
+                id="parallax"
+            ></div>
+            <img
+                src={cloud}
+                alt="cloud img"
+                className="cloud"
+            />
+            <img
+                src={cloudSoft}
+                alt="cloud-soft img"
+                className="cloud-soft"
+            />
+            <div className="content"></div>
+        </div>
+    );
 };
 
 export default Intro;
