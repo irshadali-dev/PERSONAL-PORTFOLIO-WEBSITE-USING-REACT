0f3b29fb68a0a01ec91a0a792a434df8200d20e7
Irshad Ali <ia46994@gmail.com>
2024-10-29 18:29:31 +0530

    Portfolio section create

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Intro from "./components/intro/index";
 import "./app.scss";
 import Skills from "./components/skills";
+import Portfolio from "./components/portfolio";
 
 function App() {
     return (
         <div>
             <Intro />
             <Skills />
+            <Portfolio />
         </div>
     );
 }
