102523c2c59146c3f0d14ae03c1d34a3e4b8a779
Irshad Ali <ia46994@gmail.com>
2024-12-08 16:32:19 +0530

    Cv update-

--- src/components/shared/CallToAction/index.jsx
+++ src/components/shared/CallToAction/index.jsx
@@ -2,11 +2,25 @@ import React from "react";
 import "./style.scss";
 import Arrow from "../arrow";
 
-const CallToAction = ({ text, icon, action }) => {
+const CallToAction = ({ text, icon, action, file }) => {
+    const handleClick = () => {
+        // If a file is provided, download it
+        if (file) {
+            const link = document.createElement("a");
+            link.href = file;
+            link.download = file.split("/").pop(); // Get the file name
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } else if (action) {
+            // If no file, execute the action
+            action();
+        }
+    };
     return (
         <div
             className="call-to-action"
-            onClick={action}
+            onClick={handleClick}
         >
             <span className="text">{text}</span>
 
